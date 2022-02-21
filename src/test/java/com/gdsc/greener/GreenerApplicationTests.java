b2ff5917f4cfc1243012c20de431a250a10f4389
yejin013 <yejin013@naver.com>
2022-02-21 14:37:04 +0900

    init

---
+++ src/test/java/com/gdsc/greener/GreenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.greener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
