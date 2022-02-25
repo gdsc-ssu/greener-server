98826e8f59a67019a1b8c1bc4f0bae0b74c0c24e
yejin013 <yejin013@naver.com>
2022-02-25 18:00:33 +0900

    feat: 소셜로그인 구현
    Google OAuth로 Google 소셜 로그인 개발
    
    Resolves: #1

--- src/main/java/com/gdsc/greener/controller/UserController.java
+++ src/main/java/com/gdsc/greener/controller/UserController.java
@@ -1,5 +1,25 @@
 package com.gdsc.greener.controller;
 
-public class UserController {
+import com.gdsc.greener.config.auth.SessionUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/v1")
+@RequiredArgsConstructor
+public class UserController {
+    private final HttpSession httpSession;
+
+    @GetMapping("/")
+    public HttpStatus main(){
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        return HttpStatus.OK;
+    }
 }
--- src/main/java/com/gdsc/greener/repository/UserRepository.java
+++ src/main/java/com/gdsc/greener/repository/UserRepository.java
@@ -1,4 +1,13 @@
 package com.gdsc.greener.repository;
 
-public class UserRepository {
+import com.gdsc.greener.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    // 이메일로 검색
+    Optional<User> findByEmail(String email);
 }
